752a0180e7fe62166ffc0c4f3911377e4b27503f
zhangzeyang <zhangzeyang@huidr.com>
2020-08-27 14:00:18 +0800

    一页一题

--- pages/SDS/SDS.js
+++ pages/SDS/SDS.js
@@ -5,9 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    allQuestionCount: 4,//题目总数
+    allQuestionCount: 5,//题目总数
     questionIndex: 1, //题目编号
     btnText: '下一题', //下一题/保存 显示
+    isSave:false,//是否保存 
+    questionContent: ["这些症状已经影响到了您的工作/学习", "这些症状已经影响到了您的日常生活/娱乐活动", "这些症状已经影响到了您的您的家庭生活/家庭责任", "过去的一周中，有几天是由于症状而导致您不能上学或工作，或者导致您不能承担日常职责","过去的一周中，有几天您明显受到症状的影响，导致您上学或工作时效率明显下降"]
   },
 
 
@@ -23,6 +25,9 @@ Page({
 
     if (this.data.allQuestionCount == index){
         //最后一题，保存数据
+        this.setData({
+          isSave:true
+        })
     }else{
       //不是最后一题，显示下一题，题号加1
       index+=1;
@@ -45,9 +50,28 @@ Page({
   up: function() {
     var index = this.data.questionIndex
     index-=1
+
+
+    // if(index<this.data.allQuestionCount){
+
+    // }else{
+
+    // }
+
     this.setData({
-      questionIndex: index
+      questionIndex: index,
+      btnText: '下一题'
     })
+
+    // if(index!=1){
+    //   this.setData({
+    //     questionIndex: index
+    //   })
+    // }else{
+      
+    // }
+
+    
     console.log("题目编号---上一题" + index)
   },
 
