1515d6e4a84ee3ef3b16abc2da48089150cb2d9c
zhangzeyang <zhangzeyang@huidr.com>
2020-08-27 15:02:33 +0800

    题目选择完成

--- pages/SDS/SDS.js
+++ pages/SDS/SDS.js
@@ -5,51 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-    allQuestionCount: 5,//题目总数
+    allQuestionCount: 5, //题目总数
     questionIndex: 1, //题目编号
     btnText: '下一题', //下一题/保存 显示
-    isSave:false,//是否保存 
-    questionContent: ["这些症状已经影响到了您的工作/学习", "这些症状已经影响到了您的日常生活/娱乐活动", "这些症状已经影响到了您的您的家庭生活/家庭责任", "过去的一周中，有几天是由于症状而导致您不能上学或工作，或者导致您不能承担日常职责","过去的一周中，有几天您明显受到症状的影响，导致您上学或工作时效率明显下降"]
+    isSave: false, //是否保存 
+    questionContent: ["这些症状已经影响到了您的工作/学习", "这些症状已经影响到了您的日常生活/娱乐活动", "这些症状已经影响到了您的您的家庭生活/家庭责任", "过去的一周中，有几天是由于症状而导致您不能上学或工作，或者导致您不能承担日常职责", "过去的一周中，有几天您明显受到症状的影响，导致您上学或工作时效率明显下降"],
+    days: [0, 1, 2, 3, 4, 5, 6, 7],
+
+    ansArr:[20,20,20,0,0],//答案结果 
+    
   },
 
 
 
   //下一题 
   down: function() {
-    
 
-    var index=this.data.questionIndex
+
+    var index = this.data.questionIndex
     var all = this.data.allQuestionCount
 
 
 
-    if (this.data.allQuestionCount == index){
-        //最后一题，保存数据
-        this.setData({
-          isSave:true
-        })
-    }else{
+    if (this.data.allQuestionCount == index) {
+      //最后一题，保存数据
+      this.setData({
+        isSave: true
+      })
+    } else {
       //不是最后一题，显示下一题，题号加1
-      index+=1;
-      if (index == all){
+      index += 1;
+      if (index == all) {
         this.setData({
-          questionIndex:index,
-          btnText:'保存'
+          questionIndex: index,
+          btnText: '保存'
         })
-      }else{
+      } else {
         this.setData({
-          questionIndex: index          
+          questionIndex: index
         })
       }
     }
 
-    console.log("题目编号---下一题"+index)
+    console.log("题目编号---下一题" + index)
 
   },
   //上一题 
   up: function() {
     var index = this.data.questionIndex
-    index-=1
+    index -= 1
 
 
     // if(index<this.data.allQuestionCount){
@@ -62,18 +66,30 @@ Page({
       questionIndex: index,
       btnText: '下一题'
     })
-
-    // if(index!=1){
-    //   this.setData({
-    //     questionIndex: index
-    //   })
-    // }else{
-      
-    // }
-
-    
     console.log("题目编号---上一题" + index)
   },
+
+
+
+  // 选择天数
+  doCheckDay:function(e){
+    console.log(e.detail.value)
+    var queIndex = this.data.questionIndex-1
+    var index = e.detail.value
+    this.setData({
+      ['ansArr['+queIndex+']']:index, 
+    })
+  },
+
+  // //选择程度
+  // doSelectLevel:function(e){
+  //   console.log("选择程度"+e.detail.value)  //这里是选择答案的Index
+  //   var queIndex = this.data.questionIndex - 1
+  //   var index = e.detail.value
+  //   this.setData({
+  //     ['ansArr[' + queIndex + ']']: index, 
+  //   })
+  // },
 
 
   /**
--- pages/test/test.js
+++ pages/test/test.js
@@ -5,83 +5,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    test: [1, 2, 3, 4, 5, 6, 7],
+    checkIndex:0,
   },
 
 
-  testbindcontact:function(e){
+  testbindcontact: function(e) {
     console.log("-------contact")
   },
 
-  testgetphone:function(e){
+  testgetphone: function(e) {
     console.log("获取手机号")
     console.log(e.detail)
     console.log(e)
     console.log(e.detail.encryptedData)
 
-    
+
+  },
+
+  bindpick: function(e) {
+    console.log(e.detail.value)
+    var index=e.detail.value;
+    console.log("------"+this.data.test[index])
+    this.setData({
+      checkIndex:index
+    })
   },
 
 
-  bindgetuserinfo:function(e){
+  bindgetuserinfo: function(e) {
     console.log(e)
     console.log(e.detail.rawData)
-    
+
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
